093ab5ee43afbcb3102a0a52ecd7b8cfb497a9a9
Ali Mohamed <ali.hgg@gmail.com>
2020-02-12 23:32:53 +0200

    added home component and simple Slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from './components/home/home.component';
+import { SliderComponent } from './components/home/slider/slider.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, SliderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
