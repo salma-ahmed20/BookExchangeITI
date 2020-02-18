6a0e860394cda7585a7cb7a5245cedaa8baf818e
Ali Mohamed <ali.hgg@gmail.com>
2020-02-18 17:36:03 +0200

    fix login and signup

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -7,17 +7,17 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   styleUrls: ["./login-page.component.css"]
 })
 export class LoginPageComponent {
-  // form = new FormGroup({
-  //   email: new FormControl("", [
-  //     Validators.required
-  //     // Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"),
-  //   ]),
-  //   Password: new FormControl("", Validators.required)
-  // });
-  // Email() {
-  //   return this.form.get("email");
-  // }
-  // Password() {
-  //   return this.form.get("Password");
-  // }
+  form = new FormGroup({
+    email: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$")
+    ]),
+    Password: new FormControl("", Validators.required)
+  });
+  Email() {
+    return this.form.get("email");
+  }
+  Password() {
+    return this.form.get("Password");
+  }
 }
