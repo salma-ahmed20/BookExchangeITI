d1eb2a27ce515d2e01958d4706f516e9aafb5799
Ali Mohamed <ali.hgg@gmail.com>
2020-02-18 10:50:05 +0200

    zzz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
+import { LoginPageComponent } from "./components/login-page/login-page.component";
 import { HomeComponent } from "./components/home/home.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-
-
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
+  { path: "login", component: LoginPageComponent },
   { path: "**", pathMatch: "full", component: NotFoundComponent }
 ];
 
@@ -16,5 +16,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-
-
